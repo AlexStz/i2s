697733c01ec18ba6efbca3838ee7dcac9c9a8c95
Alex <stz.alex@gmail.com>
2018-08-11 12:34:29 +0200

    Привожу прогу в нормальный вид

--- iiko2sp.py
+++ iiko2sp.py
@@ -66,91 +66,95 @@ def SHGROUPS_row_to_xml(df):
     return res
 
 
-df = pd.read_csv("./DATA/goods.csv", delimiter=";", decimal=",", header=0,
-                 converters={u'Код': convert2int32, u'Код группы': convert2int32},
-                 usecols=[u'Тип', u'Код', u'Код группы', u'Наименование', u'Ед.изм', u'Вес ед. изм', u'Цена',
-                          u'Категория'])
-# dtypess = df.dtypes
-# df[u'Код'] = convert2int32(df[u'Код'])
-# df[u'Код группы'] = convert2int32(df[u'Код группы'])
-# print(dtypess)
-# print(df.info(memory_usage='deep'))
+def main():
 
-# ищем дубли
-# print(df[df.duplicated(keep=False, subset=u'Код') == True].sort_values(by='Код'))  # todo разобраться с дублями!!!
-# print (df[u'Код'].value_counts().reset_index().query("Код > 1").rename(columns={'index':'Код', 'Код':'count'}))
-# print(df[df[u'Код'] == 1000007376].values)
-# закончили искать дубли
+    df = pd.read_csv("./DATA/goods.csv", delimiter=";", decimal=",", header=0,
+                     converters={u'Код': convert2int32, u'Код группы': convert2int32},
+                     usecols=[u'Тип', u'Код', u'Код группы', u'Наименование', u'Ед.изм', u'Вес ед. изм', u'Цена',
+                              u'Категория'])
+    # dtypess = df.dtypes
+    # df[u'Код'] = convert2int32(df[u'Код'])
+    # df[u'Код группы'] = convert2int32(df[u'Код группы'])
+    # print(dtypess)
+    # print(df.info(memory_usage='deep'))
 
-# print(sl)
-dfGroups = df[df[u'Тип'] == u"Группа"].fillna(0)  # это выборка групп
-dfItems = df[df[u'Тип'] != u"Группа"]  # это выборка товаров
-# print(dfGroups)
+    # ищем дубли
+    # print(df[df.duplicated(keep=False, subset=u'Код') == True].sort_values(by='Код'))  # todo разобраться с дублями!!!
+    # print (df[u'Код'].value_counts().reset_index().query("Код > 1").rename(columns={'index':'Код', 'Код':'count'}))
+    # print(df[df[u'Код'] == 1000007376].values)
+    # закончили искать дубли
 
-# РАБОТАЕМ С ГРУППАМИ
-# посмотрим на дубли кодов
-duplicates = dfGroups[dfGroups.duplicated(keep=False, subset=u'Код') == True].sort_values(by=u'Код')
-if len(duplicates) is not 0:
-    print("Посмотрим на дубли:")
-    print(duplicates)  # todo разобраться с дублями!!!
-    print("...дубли закончились. Выход.")
-    exit
-# /
-dfGroups.drop(columns=['Тип', 'Ед.изм', 'Вес ед. изм', 'Цена', 'Категория'], inplace=True)
-dfGroups.rename(index=str, columns={"Код": "CODE", "Код группы": "PARENT", "Наименование": "NAME"}, inplace=True)
+    # print(sl)
+    dfGroups = df[df[u'Тип'] == u"Группа"].fillna(0)  # это выборка групп
+    # dfItems = df[df[u'Тип'] != u"Группа"]  # это выборка товаров
+    # print(dfGroups)
 
-# print(dfGroups.loc[:,['CODE','PARENT']])
+    # РАБОТАЕМ С ГРУППАМИ
+    # посмотрим на дубли кодов
+    duplicates = dfGroups[dfGroups.duplicated(keep=False, subset=u'Код') == True].sort_values(by=u'Код')
+    if len(duplicates) is not 0:
+        print("Посмотрим на дубли:")
+        print(duplicates)  # todo разобраться с дублями!!!
+        print("...дубли закончились. Выход.")
+        exit
+    # /
+    dfGroups.drop(columns=['Тип', 'Ед.изм', 'Вес ед. изм', 'Цена', 'Категория'], inplace=True)
+    dfGroups.rename(index=str, columns={"Код": "CODE", "Код группы": "PARENT", "Наименование": "NAME"}, inplace=True)
 
-dfGroups.set_index('CODE', inplace=True)
-# print(dfGroups)
-dfGroupsChildCount = dfGroups.groupby("PARENT")["PARENT"].count().rename("CCOUNT").reset_index()\
-    .rename(index=str, columns={'PARENT': 'CODE'}).set_index('CODE')
-# print(dfGroupsChildCount)
-# dfGroups['count'] = dfGroups.groupby("Код группы")["Код группы"].transform(len)
-# print(dfGroups)
-dfGroupsWithCCOUNT = dfGroups.join(dfGroupsChildCount).fillna(0)
-dfGroupsWithCCOUNT["CCOUNT"] = dfGroupsWithCCOUNT["CCOUNT"].astype(int)
-# print(dfGroupsWithCCOUNT)
+    # print(dfGroups.loc[:,['CODE','PARENT']])
 
-# заполняем FULLPATH
-dfGroupsWithCCOUNT["FULLPATH"] = ""
+    dfGroups.set_index('CODE', inplace=True)
+    # print(dfGroups)
+    dfGroupsChildCount = dfGroups.groupby("PARENT")["PARENT"].count().rename("CCOUNT").reset_index()\
+        .rename(index=str, columns={'PARENT': 'CODE'}).set_index('CODE')
+    # print(dfGroupsChildCount)
+    # dfGroups['count'] = dfGroups.groupby("Код группы")["Код группы"].transform(len)
+    # print(dfGroups)
+    dfGroupsWithCCOUNT = dfGroups.join(dfGroupsChildCount).fillna(0)
+    dfGroupsWithCCOUNT["CCOUNT"] = dfGroupsWithCCOUNT["CCOUNT"].astype(int)
+    # print(dfGroupsWithCCOUNT)
+
+    # заполняем FULLPATH
+    dfGroupsWithCCOUNT["FULLPATH"] = ""
+
+    # for i in range(len(dfGroupsWithCCOUNT)):
+    #     print(dfGroupsWithCCOUNT[i].index.astype(str)+'.')
+
+    def getParentPath(index):
+        path = str(index) + "."
+        ParentIndex = dfGroupsWithCCOUNT.loc[index, ['PARENT']]["PARENT"]
+        if ParentIndex == 0:
+            return path
+        else:
+            ppath = getParentPath(ParentIndex)
+        return ppath + path
+
+    for index, row in dfGroupsWithCCOUNT.iterrows():
+        path = getParentPath(index)
+        dfGroupsWithCCOUNT.at[index, 'FULLPATH'] = path
+        # print(path)
+
+    dfGroupsWithCCOUNT.reset_index(inplace=True)
+    # tempDF = dfGroupsWithCCOUNT.loc[:,["FULLPATH"]].apply(lambda x:  )
+    # print(tempDF)
+    # /
+
+    # сортируем
+    # print(dfGroupsWithCCOUNT.sort_values(by=['FULLPATH']))
+    dfGroupsWithCCOUNT.sort_values(by=['FULLPATH'], inplace=True)
+    # /
+
+    # dfGroups['Код'].
+    # dfGroups.loc['Код']
+    # print(dfGroups)
+    # print(dfGroups.loc[:,('Код','Код группы','count')])
+    print(SHGROUPS_xml_header() + SHGROUPS_row_to_xml(dfGroupsWithCCOUNT) + SHGROUPS_xml_ending())
+    with open("./DATA/SHGROUPS.XML", 'w') as f:
+        f.write(SHGROUPS_xml_header() + SHGROUPS_row_to_xml(dfGroupsWithCCOUNT) + SHGROUPS_xml_ending())
+    # /РАБОТАЕМ С ГРУППАМИ
+    # input()
 
 
-def getParentPath(index):
-    path = str(index) + "."
-    ParentIndex = dfGroupsWithCCOUNT.loc[index, ['PARENT']]["PARENT"]
-    if ParentIndex == 0:
-        return path
-    else:
-        ppath = getParentPath(ParentIndex)
-    return ppath + path
-
-
-# for i in range(len(dfGroupsWithCCOUNT)):
-#     print(dfGroupsWithCCOUNT[i].index.astype(str)+'.')
-
-for index, row in dfGroupsWithCCOUNT.iterrows():
-    path = getParentPath(index)
-    dfGroupsWithCCOUNT.at[index, 'FULLPATH'] = path
-    # print(path)
-
-
-dfGroupsWithCCOUNT.reset_index(inplace=True)
-# tempDF = dfGroupsWithCCOUNT.loc[:,["FULLPATH"]].apply(lambda x:  )
-# print(tempDF)
-# /
-
-# сортируем
-# print(dfGroupsWithCCOUNT.sort_values(by=['FULLPATH']))
-dfGroupsWithCCOUNT.sort_values(by=['FULLPATH'], inplace=True)  # TODO здесь проблемка - буду думать
-# /
-
-# dfGroups['Код'].
-# dfGroups.loc['Код']
-# print(dfGroups)
-# print(dfGroups.loc[:,('Код','Код группы','count')])
-print(SHGROUPS_xml_header() + SHGROUPS_row_to_xml(dfGroupsWithCCOUNT) + SHGROUPS_xml_ending())
-with open("./DATA/SHGROUPS.XML", 'w') as f:
-    f.write(SHGROUPS_xml_header() + SHGROUPS_row_to_xml(dfGroupsWithCCOUNT) + SHGROUPS_xml_ending())
-# /РАБОТАЕМ С ГРУППАМИ
-# input()
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
